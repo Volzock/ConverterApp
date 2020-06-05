0f0ad817f2dc65a78f46bccdcee6e95fd8ffa7d1
Volzock <nukfeed@yandex.ru>
2020-06-05 11:49:03 +0300

    Переделан UI, теперь output отображается в отдельном объекте

--- app/src/main/java/com/example/converterapp/MainActivity.kt
+++ app/src/main/java/com/example/converterapp/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         btn.setOnClickListener {
             try {
-                et.setText((et.text.toString().toDouble()*74).toString())
+                et_to.text = (et_from.text.toString().toDouble()*74).toString()+"rub"
             } catch (e: NumberFormatException) {
                 Toast.makeText(applicationContext, "Number is wrong, please try again", Toast.LENGTH_SHORT).show()
             }
