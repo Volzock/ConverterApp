333b109c4811e4babb7d2094d1000a99ff510120
Volzock <nukfeed@yandex.ru>
2020-06-05 10:51:43 +0300

    Решает проблему с маленькими устройствами, теперь на любом размере экрана будет работать так, как запланировано

--- app/src/main/java/com/example/converterapp/MainActivity.kt
+++ app/src/main/java/com/example/converterapp/MainActivity.kt
@@ -8,7 +8,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
-
+    // testing on pixel2, pixel3a
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
