750ae5f9dbdb28ee0af07958ad0667bc43e6d1bd
Volzock <nukfeed@yandex.ru>
2020-06-05 16:42:27 +0300

    Переработан UI, выполнены функциональные требования третьего этапа

--- app/src/main/java/com/example/converterapp/MainActivity.kt
+++ app/src/main/java/com/example/converterapp/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.example.converterapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
 import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,17 +10,22 @@ import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
     // testing on pixel2, pixel3a, 2.7smartphone
+    val cur = mutableMapOf<String, String>("RUB" to "1.0", "EUR" to "77.8", "USD" to "68.7", "GBP" to "86.3", "UAH" to "2.6", "JPY" to "0.6", "CNY" to "9.6")
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btn.setOnClickListener {
             try {
-                et_to.text = (et_from.text.toString().toDouble()*(if (ch.selectedItem != "RUB") 74.0 else 1.0/74)).toString()+" ${if (ch.selectedItem == "RUB") "USD" else "RUB"}"
-            } catch (e: NumberFormatException) {
+                // et_to.text = (et_from.text.toString().toDouble()*(cur[from_cur.selectedItem]?.toDouble() ?: 1)*).toString()+" ${to_cur.selectedItem}"
+                et_to.text = (et_from.text.toString().toDouble() * (cur[from_cur.selectedItem]?.toDouble() ?: 1.0) / (cur[to_cur.selectedItem]?.toDouble() ?: 1.0)).toString() + " ${to_cur.selectedItem}"
+            } catch(e: NumberFormatException) {
                 Toast.makeText(applicationContext, "Number is wrong, please try again", Toast.LENGTH_SHORT).show()
             }
         }
+
     }
 
 }
